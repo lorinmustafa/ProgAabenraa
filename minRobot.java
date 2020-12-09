86fb18bb3b7ae8d588ed70a3a880cb19c5d3b815
chrisEUCSYD <chb@eucsyd.dk>
2020-12-09 11:10:58 +0100

    MIn Robot
    First update

---
+++ minRobot.java
@@ -0,0 +1,20 @@
+package ProgAabenraa;
+
+import robocode.Robot;
+
+public class minRobot extends Robot {
+    @java.lang.Override
+    public void run() {
+        ahead(100);
+        turnGunLeft(90);
+    }
+
+    public static class minRobot extends Robot {
+        @java.lang.Override
+        public void run() {
+            ahead(100);
+            turnGunLeft(90);
+        }
+    }
+}
+
